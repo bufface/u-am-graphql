d08eefc17ab1d7faae04bae9d6ccee94561d2936
Cristian Buffa <bufface@gmail.com>
2020-02-08 10:32:21 -0300

    7. Creating Your Own GraphQL API

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,36 @@
-console.log('Hello GraphQL');
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String
+  }
+`
+
+// Resolvers
+const resolvers = {
+  Query:{
+    hello() {
+      return 'This is my first query!'
+    },
+    name() {
+      return 'Cristian Buffa'
+    },
+    location() {
+      return 'Buenos Aires'
+    },
+    bio() {
+      return 'Some random string!'
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+server.start(() => console.log('The server is up!'))
